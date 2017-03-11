374113b96ffe22f4d85c4e4f01e44566d22613e8
Jeremy Zornow <jeremy@zornow.com>
2017-03-11 12:30:13 -0700

    Add two- and three-letter string cases

--- lib/permutation_generator.rb
+++ lib/permutation_generator.rb
@@ -7,7 +7,24 @@ class PermutationGenerator
   end
 
   def list_permutations
-    [@phrase]
+    permutations_for(@phrase).flatten
+  end
+
+  def permutations_for(input_string)
+    if input_string.length == 1
+      [input_string]
+    else
+      input_string.chars.map do |character|
+        sub_permutations = permutations_for input_string.delete(character)
+        distribute_letter(character, sub_permutations.flatten)
+      end
+    end
+  end
+
+  def distribute_letter(letter, array_of_strings)
+    array_of_strings.map do |string|
+      letter + string
+    end
   end
 end
 
--- spec/permutation_generator_spec.rb
+++ spec/permutation_generator_spec.rb
@@ -12,7 +12,23 @@ describe PermutationGenerator do
       end
     end
 
-        expect(generator.list_permutations).to eq([phrase])
+    context 'given a string with two letters' do
+      it 'returns both valid permutations' do
+        phrase = 'ab'
+        generator = PermutationGenerator.new(phrase)
+
+        expect(generator.list_permutations)
+          .to match_array(%w(ab ba))
+      end
+    end
+
+    context 'given a string with three letters' do
+      it 'returns all valid permutations' do
+        phrase = 'abc'
+        generator = PermutationGenerator.new(phrase)
+
+        expect(generator.list_permutations)
+          .to match_array(%w(abc acb bac bca cab cba))
       end
     end
   end
